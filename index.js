595b87761b52ff0d559fe1fd7ae8b1651379feff
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2024-11-11 17:06:06 +0000

    Callback functions example

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-
+const api={
+    createOrder:function(cart,generateSummary){
+        setTimeout(()=>{
+            console.log('This are the cart items ' + cart);
+            console.log('Order is Created for you!');
+            generateSummary();
+        },1000)
+    },
+    orderSummary:function(itemsInOrder,paymentOpt){
+        setTimeout(()=>{
+            console.log('Order Summary is created for you!',+ itemsInOrder);
+            paymentOpt();
+        },1000)
+    },
+    paymentDetails:function(){
+        setTimeout(()=>{
+            console.log('Payment Done Successfully!');
+        },1000)
+    }
+}
+const cart=['shirt','pant','shoes'];
+//api to create order
+api.createOrder(cart,()=>{
+    api.orderSummary(cart,()=>{
+        api.paymentDetails();// the code is growing horizontally aka inversion of control & pyramid of doom
+    });
+})
